ba98550a42b0ce6cf70ed6aacff6e5a59907da91
lab314-21@labeng.local <lab314-21@labeng.local>
2020-01-10 14:53:44 +0700

    send

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string name;
+    double grade;
+    cout << "What is your name?: ";
+    cin >> name;
+    cout << "What is your GPA?: ";
+    cin >> grade;
+    if(grade >= 3.5){
+        cout << name << " InW Jrim Jrim! ! !";
+    }
+    else
+    {
+        cout << "Try harder, " << name << "! ! !" ;
+    }
+    return 0;
+}
